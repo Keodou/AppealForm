a96b8c8d9042f8ca2279c334098ee77d89703e83
Dmitry Korobeyshchikov <korobeyshchikov.d@mail.ru>
2023-04-20 17:50:16 +0300

    Made CRUD and http queries for messages

---
+++ src/AppealForm/AppealForm.DAL/Data/AppealFormDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AppealForm.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppealForm.DAL.Data
+{
+    public class AppealFormDbContext : DbContext
+    {
+        public AppealFormDbContext(DbContextOptions<AppealFormDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ src/AppealForm/AppealForm.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,89 @@
+﻿using AppealForm.DAL.Data;
+using AppealForm.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppealForm.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly AppealFormDbContext _dbContext;
+
+        public MessagesController(AppealFormDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Message>> PostMessage(Message message)
+        {
+            _dbContext.Messages.Add(message);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMessage), new { id = message.Id }, message);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Message>> GetMessage(Guid id)
+        {
+            var message = await _dbContext.Messages.FindAsync(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return message;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage(Guid id, Message message)
+        {
+            if (id != message.Id)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!MessageItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessage(Guid id)
+        {
+            var message = await _dbContext.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Messages.Remove(message);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MessageItemExists(Guid id)
+        {
+            return _dbContext.Messages.Any(m => m.Id == id);
+        }
+    }
+}
